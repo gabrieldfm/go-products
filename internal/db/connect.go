9b5998bcdad598c44699826ef743589cf3682692
gabrieldfm <gabrieldfm13@gmail.com>
2025-04-08 22:55:35 -0300

    removing commetns

--- internal/db/connect.go
+++ internal/db/connect.go
@@ -3,17 +3,19 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func getConnection() (client *mongo.Client, ctx context.Context) {
-	/*errEnv := godotenv.Load()
+	errEnv := godotenv.Load()
 	if errEnv != nil {
 		log.Fatal("Error loading .env file: ", errEnv)
-	}*/
+	}
 
 	connectionString := os.Getenv("CONECTIONSTRING_MONGO")
 
